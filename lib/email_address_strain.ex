cefc938978e6312fec3a309834c44de13cde02c1
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2019-08-13 20:42:52 +0100

    creating empty project

---
+++ lib/email_address_strain.ex
@@ -0,0 +1,18 @@
+defmodule EmailAddressStrain do
+  @moduledoc """
+  Documentation for EmailAddressStrain.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EmailAddressStrain.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
